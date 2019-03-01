6bee2b6023740e8a02fd7c4751bec8e4c25baa2f
apk <apkd@users.noreply.github.com>
2019-03-02 00:06:19 +0100

    Add APKD_STACKTRACE_NOCACHE option

--- src/CacheDictionary.cs
+++ src/CacheDictionary.cs
@@ -9,11 +9,20 @@ namespace Apkd.Internal
         readonly System.Runtime.CompilerServices.ConditionalWeakTable<TKey, TValue> weakTable
             = new System.Runtime.CompilerServices.ConditionalWeakTable<TKey, TValue>();
 
+        const int defaultCacheSize = 256;
+
         public CacheDictionary(int cacheSize = 256)
+#if APKD_STACKTRACE_NOCACHE
+            { }
+#else
             => (this.cacheSize, objectReferenceQueue) = (cacheSize, new Queue<object>(capacity: cacheSize));
+#endif
 
         public TValue GetOrInitializeValue(TKey key, System.Func<TKey, TValue> initializer)
         {
+#if APKD_STACKTRACE_NOCACHE
+            return initializer(key);
+#else
             if (!weakTable.TryGetValue(key, out var value))
                 weakTable.Add(key, value = initializer(key));
                 
@@ -22,6 +31,7 @@ namespace Apkd.Internal
             objectReferenceQueue.Enqueue(value);
 
             return value;
+#endif
         }
     }
 }
